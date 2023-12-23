fe08719662ada2ec7c33386ab594d927918f3174
CH_Hwang <hch950627@naver.com>
2023-12-24 00:49:08 +0900

    리트코드 160번

---
+++ ReetCode/160. Intersection of Two Linked Lists/index.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+const getIntersectionNode = function (headA, headB) {};
